dfcd34da7a1d9bde54407db10948ed8e9e93b05c
Nicole Trindade <nicole.trindade@poli.ufrj.br>
2021-11-19 00:35:13 -0300

    adicionando linhas de tabela pelo script

--- script.js
+++ script.js
@@ -14,27 +14,27 @@ const Modal= {
 
     }
 }
-const transactions =[{
-        id= 1,
-        description= 'Luz',
-        amount= -30000,
-        date= 01/02/2022
+const transactions = [{
+        id: 1 ,
+        description: 'Luz',
+        amount: -30000,
+        date: '01/02/2022',
     },
     {    
-        id= 2,
-        description= 'website',
-        amount= 500000,
-        date= 01/02/2022},
+        id: 2 ,
+        description: 'website',
+        amount: 500000,
+        date: '01/02/2022'},
     {    
-        id= 3,
-        description= 'Água',
-        amount= -20000,
-        date= 01/02/2022},
+        id: 3 ,
+        description: 'Água',
+        amount: -20000,
+        date: '01/02/2022'},
     {    
-        id= 4,
-        description='Aluguel',
-        amount= -130000,
-        date= 01/02/2022}
+        id: 4 ,
+        description:'Aluguel',
+        amount: -130000,
+        date: '01/02/2022'}
     ]
 const Transaction = {
     incomes (){
@@ -48,23 +48,30 @@ const Transaction = {
     }
 }
 const DOM = {
-            addTransaction (transaction, index){
-                const tr = document.createElement('tr')
-                tr.innerHTML = DOM.innerHTMLTransaction()
+        transactionsContainer: document.querySelector('#data-table tbody'),
+            
+        addTransaction(transaction, index){
+
+            const tr = document.createElement('tr')
+            tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+            DOM.transactionsContainer.appendChild(tr)
+
 
         },
-            InnerHTMLTransaction (transaction){
-        const html = `
-        <tr>
-            <td class="description">Luz</td>
-            <td class="expense"> -R$300,00</td>
-            <td class="date">01/02/2022</td>
-            <td>
-                <img src="./assets/minus.svg" alt="remover transação">
-            </td>
-        </tr>
-        `
-        return html
+
+        innerHTMLTransaction(transaction){
+            const html = `
+            <tr>
+                <td class="description">${transaction.description}</td>
+                <td class="expense"> ${transaction.amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td>
+                    <img src="./assets/minus.svg" alt="remover transação">
+                </td>
+            </tr>
+            `
+            return html
     }
 }
 DOM.addTransaction(transactions[0])
