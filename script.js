6f030ddd744bdc985d4ed3f377761cd83d90a3d9
Nicole Trindade <nicole.trindade@poli.ufrj.br>
2021-11-20 01:18:57 -0300

    feat : função que reconhece valores de transação

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const transactions = [{
     },
     {    
         id: 2 ,
-        description: 'website',
+        description: 'Website',
         amount: 500000,
         date: '01/02/2022'},
     {    
@@ -49,8 +49,17 @@ const Transaction = {
 }
 
 const Utils = {
-    formatCurrency(){
-        
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : "" 
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
     }
 }
 const DOM = {
@@ -68,10 +77,12 @@ const DOM = {
 
         innerHTMLTransaction(transaction){
             const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+            const amount = Utils.formatCurrency(transaction.amount)
             const html = `
             <tr>
                 <td class="description">${transaction.description}</td>
-                <td class=${CSSclass} > ${transaction.amount}</td>
+                <td class=${CSSclass} > ${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
                     <img src="./assets/minus.svg" alt="remover transação">
