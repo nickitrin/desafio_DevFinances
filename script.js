dd48f9a1af0a80c7aacfd6953951b0fd1c8f0b02
Nicole Trindade <nicole.trindade@poli.ufrj.br>
2021-11-22 20:40:07 -0300

    feat: mensagem de erro e validação de campos

--- script.js
+++ script.js
@@ -126,8 +126,42 @@ const Utils = {
     }
 }
 const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues(){
+        return{ 
+        description: Form.description.value,
+        amount: Form.amount.value,
+        date: Form.date.value}
+    },
+    formatData(){
+        console.log('formatar dados')
+
+    },
+    validateFields(){  
+            const {description, amount, date} = Form.getValues()      
+            if (
+                description.trim() === '' || 
+                amount.trim() === '' || 
+                date.trim() === '') 
+            {
+            throw new Error("Por favor, preencha todos os campos")
+            }
+    },
     submit(event){
+
         event.preventDefault()
+
+        try {
+             Form.validateFields()
+        } catch (error) {
+            alert(error.message)
+        }
+
+        Form.formatData()
+    
     }
 }
 const App = {
